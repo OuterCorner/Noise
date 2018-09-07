90e1fe282c314b99bb1fd5f31f51a01ff188dfdb
Paulo Andrade <paulo@outercorner.com>
2018-09-07 17:36:35 +0100

    Initial Commit

---
+++ Noise/Noise.h
@@ -0,0 +1,19 @@
+//
+//  Noise.h
+//  Noise
+//
+//  Created by Paulo@OC on 07/09/2018.
+//  Copyright © 2018 Outer Corner. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Noise.
+FOUNDATION_EXPORT double NoiseVersionNumber;
+
+//! Project version string for Noise.
+FOUNDATION_EXPORT const unsigned char NoiseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Noise/PublicHeader.h>
+
+
