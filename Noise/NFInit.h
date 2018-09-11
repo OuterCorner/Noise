fa868ab961a8c3fd20c7121af29af3676aaf6a05
Paulo Andrade <paulo@outercorner.com>
2018-09-11 20:02:28 +0100

    Initializing Noise-C on framework load

---
+++ Noise/NFInit.h
@@ -0,0 +1,9 @@
+//
+//  NFInit.h
+//  Noise
+//
+// Created by Paulo Andrade on 11/09/2018.
+// Copyright © 2018 Outer Corner. All rights reserved.
+//
+
+void NFInit(void) __attribute__((constructor));
---
+++ Noise/NFInit.m
@@ -0,0 +1,14 @@
+//
+//  NFInit.m
+//  Noise
+//
+// Created by Paulo Andrade on 11/09/2018.
+// Copyright © 2018 Outer Corner. All rights reserved.
+//
+
+#import "NFInit.h"
+#import <noise/protocol.h>
+
+void NFInit(void) {
+    noise_init();
+}
