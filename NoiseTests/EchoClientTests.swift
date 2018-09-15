31fb7c06513aa0d15a3e76ab48ac13ebbd865630
Paulo Andrade <paulo@outercorner.com>
2018-09-15 07:46:45 +0100

    Started work on EchoClientTests

---
+++ NoiseTests/EchoClientTests.swift
@@ -0,0 +1,30 @@
+//
+//  EchoClientTests.swift
+//  Noise
+//
+// Created by Paulo Andrade on 14/09/2018.
+// Copyright © 2018 Outer Corner. All rights reserved.
+//
+
+import XCTest
+import Noise
+
+class EchoClientTests: XCTestCase {
+
+    override func setUp() {
+        // run the server
+    }
+
+    override func tearDown() {
+        // stop the server
+    }
+
+    func testNN() {
+        
+    }
+
+    func testXX() {
+        
+    }
+
+}
